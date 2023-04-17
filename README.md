f97f020b29a88eb97343c8f1e9c77128baa3e81d
Replit user <>
2023-04-17 01:17:51 +0000

    Primeira parte da atualização

---
+++ README.md
@@ -0,0 +1,17 @@
+# Meu portfólio em HTML, CSS e JavaScript
+
+## Descrição
+Esse projeto foi feito para agrupar os outros projetos que são hospedados em diferentes plataformas (Codepen, Replit e aqui no GitHub), mantendo tudo organizando num único lugar para indexa-los e ordenando-os do mais recente para o mais antigo.
+
+## Detalhes
+- No canto superior direito há um botão para mudar os temas;
+- Os botões abaixo das thumbnails dos projetos direcionam para a documentação e para o resultado final;
+- No footer estão presentes os links úteis sobre mim.
+
+## Referências
+Tentei fazer várias pesquisas para achar referências que me inspirassem a fazer algo de acordo com minha personalidade, as minhas principais fontes de inspiração foram o site do [Takuya Matsuyama](https://www.craftz.dog/) e o site da banda [Men, I Trust (merch)](https://menitrustmerch.com/).
+
+Para os temas claro e escuro, eu decidi usar a paleta de cores de um dos meus temas favoritos: [Solarized](https://ethanschoonover.com/solarized/).
+
+## Assets
+- Icones: [Google Material Symbols](https://fonts.google.com/icons)
--- index.js
+++ index.js
@@ -1,176 +1,179 @@
-const projetos = [
-	{
-		nome: "Landing Page apenas com JavaScript puro",
-		descrição:
-			"Nesse projeto eu foquei em usar só JS para criar, estilizar e dar funções aos elementos. Ao clicar no botão ao lado do container com o texto no centro da tela, os estilos e conteúdos mudam em conjunto, esse botão tem uma animação personalizada de clique. Há um menu clicável no canto superior esquerdo com elementos animados. O footer tem uma animação igual a do menu ao passar o mouse sobre os botões.",
-		imagem:
-			"https://raw.githubusercontent.com/augusto404/portfolio/main/img/landing-page.png",
-		link: "https://codepen.io/augusto404/full/bGxyoGV"
-	},
-	{
-		nome: "Página de produto",
-		descrição:
-			"Página de produto no estilo mobile first com responsividade para desktop. Todos os elementos da página foram criados usando apenas JavaScript e CSS. Foi interessante fazer um projeto funcionar usando apenas JS para criar as tags HTML e adicionar atributos. Os botões que ficam abaixo da descrição do produto são funcionais, o de adicionar o produto ao carrinho exibe uma mensagem temporária ao ser clicado, e as setas mudam entre as diferentes opções de sneakers. A fonte de cada imagem aparece no footer da página junto com o nome do autor.",
-		imagem:
-			"https://raw.githubusercontent.com/augusto404/portfolio/main/img/pagina-de-produto.png",
-		link: "https://codepen.io/augusto404/full/KKxbrMw"
-	},
-	{
-		nome: "Tela de login e cadastro no estilo mobile first",
-		descrição:
-			'Os temas mudam automaticamente a cada cinco segundos. Os botões de Login e Criar Conta são clicáveis e exibem uma mensagem temporária que teoricamente ficaria presente no tempo entre o carregamento da página atual para a próxima.',
-		imagem:
-			"https://raw.githubusercontent.com/augusto404/portfolio/main/img/login-e-cadastro.png",
-		link: "https://codepen.io/augusto404/full/oNPQROp"
-	},
-	{
-		nome: "timer pomodoro e relógio digital",
-		descrição:
-			"Esse projeto é um relógio digital e timer pomodoro esteticamente simples para ser usado em momentos de estudo, trabalho e/ou atividades que exigem concentração por longos períodos. Por padrão, é apresentado um relógio. Ao clicar nos botões, pode ser iniciado o timer para a seção de foco ou para os minutos de descanso, além do último botão permitir que os temas sejam mudados manualmente. A cada ciclo finalizado, seja de foco ou descanso, um som de notificação toca para lembrar da conclusão da seção e o tema muda automaticamente.",
-		imagem:
-			"https://raw.githubusercontent.com/augusto404/portfolio/main/img/pomodoro%20timer.png",
-		link: "https://codepen.io/augusto404/full/ZEMrKMg"
-	},
-	{
-		nome: "link in bio",
-		descrição:
-			"Site no estilo link in bio voltado para dispositivos móveis. Uso esse site para listar os principais links úteis sobre mim, colocando na bio do Instagram, no GitHub e nas informações de contato no meu perfil aqui no LinkedIn.",
-		imagem:
-			"https://raw.githubusercontent.com/augusto404/portfolio/main/img/link-in-bio.png",
-		link: "https://codepen.io/augusto404/full/XWPEBzX"
-	},
-	{
-		nome: "cartão de crédito com animação",
-		descrição:
-			"Criei esse projeto para exercitar o que aprendi sobre armazenamento de dados em vetores e como posso utilizar funções para criar eventos dentro do documento HTML. Coloquei os conjuntos de cores dentro de funções que ficam dentro de uma array, eles modificam o plano de fundo, o cartão, e os links do footer. Adicionei algumas transições simples com CSS para que as mudanças de cores tenham um aspecto mais natural. A mudança de estilos acontece a cada cinco segundos, mas pode ser adiantada ao clicar no cartão.",
-		imagem:
-			"https://raw.githubusercontent.com/augusto404/portfolio/main/img/credit-card.png",
-		link: "https://codepen.io/augusto404/full/abaJLpQ"
-	},
-	{
-		nome: "formulário de inscrição para newsletter de astronomia",
-		descrição: "",
-		imagem:
-			"https://raw.githubusercontent.com/augusto404/portfolio/main/img/newsletter-astronomia.png",
-		link: "https://augusto404.github.io/FormularioNewsletter"
-	}
-];
-
-// criar elementos para cada projeto dentro da array
-window.addEventListener("load", () => {
-	const containerProjetos = document.querySelector(".projetos");
-
-	for (objeto in projetos) {
-		const projeto = `
-			<section title="${projetos[objeto].nome} | ${projetos[objeto].descrição}">
-				<a href="${projetos[objeto].link}" rel="external" target="_blank">
-					<img src="${projetos[objeto].imagem}" alt='${projetos[objeto].nome}'>
-				</a>
-			</section>
-		`;
-
-		containerProjetos.innerHTML += projeto;
-	}
-});
-
-// o tema padrão é claro
-// essa escolha foi feita pra aplicar as coisas que são feitas com JS assim que a página carregar
-function mudarCores(tema = "temaClaro") {
-	// paleta de cores de cada tema
+// o tema padrão é escuro
+function mudarCores(tema = "temaEscuro") {
+	// definindo paleta de cores geral
 	const cores = {
-		temaClaro: {
-			body: "#cbb798",
-			headers: "#51524b",
-			icone: "sandybrown",
-			bordaHeaders: "#51524b",
-			bordas: "black",
-			boxShadow: "black",
-			paragrafo: "olive"
-		},
-		temaEscuro: {
-			body: "#24261A",
-			headers: "#77AED9",
-			icone: "darkblue",
-			bordaHeaders: "darkgrey",
-			bordas: "#5B89A6",
-			boxShadow: "#385C73",
-			paragrafo: "#3A4031"
-		}
+		background001: "#002b36",
+		background002: "#073642",
+		background003: "#eee8d5",
+		background004: "#fdf6e3",
+		conteudo001: "#93a1a1",
+		conteudo002: "#839496",
+		conteudo003: "#657b83",
+		conteudo004: "#586e75",
+		destaques001: "#d33682",
+		destaques002: "#b58900",
+		destaques003: "#cb4b16",
+		destaques004: "#2aa198"
 	};
 
-	// ecurtando as declarações dos seletores dos elementos no html
+	// selecionando elementos
 	const elementos = {
 		body: document.querySelector("body"),
-		icone: document.querySelector(".mudar-tema"),
-		headers: document.querySelectorAll("h1, h2"),
-		paragrafo: document.querySelector(".apresentacao p"),
-		bordaCaixas: document.querySelectorAll(".projetos section, .mudar-tema"),
-		projetos: document.querySelectorAll(".projetos section")
+		header: document.querySelector("header"),
+		headerTitulo: document.querySelector("header h1"),
+		headerParagrafo: document.querySelector("header p"),
+		botãoMudarTema: document.querySelector(".mudar-tema"),
+		botãoMudarTemaIcone: document.querySelector(".mudar-tema span"),
+		projetoBackground: document.querySelectorAll(".projeto"),
+		projetoTitulo: document.querySelectorAll(".projeto h2"),
+		projetoLinks: document.querySelectorAll(".botoes a"),
+		footerLinks: document.querySelectorAll("footer a")
 	};
 
-	elementos.body.style.backgroundColor = cores[tema].body;
-	elementos.icone.style.backgroundColor = cores[tema].icone;
-
-	// cor do texto e borda iferior dos headers
-	for (let i = 0; i < elementos.headers.length; i++) {
-		elementos.headers[i].style.color = cores[tema].headers;
-		elementos.headers[i].style.borderBottom = `2px solid ${cores[tema].headers}`;
+	// criando os presets de cores dos temas
+	const presetsTemas = {
+		temaClaro: {
+			bodyTexto: cores.conteudo003,
+			bodyBackground: cores.background003,
+			headerBackground: cores.background004,
+			headerTitulo: cores.conteudo004,
+			headerParagrafo: cores.conteudo003,
+			botãoMudarTemaBackground: cores.background001,
+			botãoMudarTemaIcone: cores.destaques001,
+			projetoBackground: cores.background002,
+			projetoTitulo: cores.conteudo001,
+			projetoLinks: cores.destaques002,
+			projetoLinksBorda: `1.5px solid ${cores.destaques002}`,
+			footerLinks: cores.conteudo003
+		},
+		temaEscuro: {
+			bodyTexto: cores.conteudo002,
+			bodyBackground: cores.background001,
+			headerBackground: cores.background002,
+			headerTitulo: cores.conteudo001,
+			headerParagrafo: cores.conteudo002,
+			botãoMudarTemaBackground: cores.background004,
+			botãoMudarTemaIcone: cores.destaques002,
+			projetoBackground: cores.background003,
+			projetoTitulo: cores.destaques003,
+			projetoLinks: cores.destaques004,
+			projetoLinksBorda: `1.5px solid ${cores.destaques004}`,
+			footerLinks: cores.conteudo002
+		}
 	}
 
-	// borda dos elementos
-	for (let i = 0; i < elementos.bordaCaixas.length; i++) {
-		elementos.bordaCaixas[i].style.border = `2px solid ${cores[tema].bordas}`;
+	// aplicando cores nos elementos
+	elementos.body.style.color = presetsTemas[tema].bodyTexto;
+	elementos.body.style.backgroundColor = presetsTemas[tema].bodyBackground;
+	elementos.header.style.backgroundColor = presetsTemas[tema].headerBackground;
+	elementos.headerTitulo.style.color = presetsTemas[tema].headerTitulo;
+	elementos.headerParagrafo.style.color = presetsTemas[tema].headerParagrafo;
+	elementos.botãoMudarTema.style.backgroundColor = presetsTemas[tema].botãoMudarTemaBackground;
+	elementos.botãoMudarTemaIcone.style.color = presetsTemas[tema].botãoMudarTemaIcone;
+
+	// usando laços de repetição para aplicar estilos em vários elementos com a mesma classe
+	for (let i = 0; i < elementos.projetoBackground.length; i++) {
+		elementos.projetoBackground[i].style.backgroundColor = presetsTemas[tema].projetoBackground;
+		elementos.projetoTitulo[i].style.color = presetsTemas[tema].projetoTitulo;
+
+		for (let j = 0; j < elementos.projetoLinks.length; j++) {
+			elementos.projetoLinks[j].style.color = presetsTemas[tema].projetoLinks;
+
+			elementos.projetoLinks[j].addEventListener("mouseover", () => {
+				elementos.projetoLinks[j].style.border = presetsTemas[tema].projetoLinksBorda;
+			});
+			elementos.projetoLinks[j].addEventListener("mouseout", () => {
+				elementos.projetoLinks[j].style.border = "1.5px solid transparent";
+			});
+		}
 	}
-	elementos.paragrafo.style.border = `2px solid ${cores[tema].bordas}`;
 
-	// para cada elemento que passar o mouse, haverá esse efeito
-	elementos.paragrafo.style.background = cores[tema].paragrafo;
-	for (let i = 0; i < elementos.bordaCaixas.length; i++) {
-		elementos.bordaCaixas[i].addEventListener("mouseover", () => {
-			elementos.bordaCaixas[i].style.transform = "translate(-3px, -3px)";
-			elementos.bordaCaixas[
-				i
-			].style.boxShadow = `3px 3px ${cores[tema].boxShadow}`;
-		});
-		elementos.bordaCaixas[i].addEventListener("mouseout", () => {
-			elementos.bordaCaixas[i].style.transform = "translate(0px, 0px)";
-			elementos.bordaCaixas[i].style.boxShadow = "0px 0px transparent";
-		});
-
-		// efeito do clique no botão de tema e nos projetos
-		elementos.bordaCaixas[i].addEventListener("click", () => {
-			elementos.bordaCaixas[i].style.transform = "translate(0px, 0px)";
-			elementos.bordaCaixas[i].style.boxShadow = "0px 0px transparent";
-
-			setTimeout(() => {
-				elementos.bordaCaixas[i].style.transform = "translate(-3px, -3px)";
-				elementos.bordaCaixas[
-					i
-				].style.boxShadow = `3px 3px ${cores[tema].boxShadow}`;
-			}, 200);
-		});
+	for (let i = 0; i < elementos.footerLinks.length; i++) {
+		elementos.footerLinks[i].style.color = presetsTemas[tema].footerLinks;
 	}
 }
 
 // mudar icone ao clicar no botão
 const botão = document.querySelector(".mudar-tema");
 const icone = document.querySelector(".mudar-tema span");
-let temaSelecionado = "";
 
 function mudarIcone() {
 	if (icone.classList.contains("tema-noite")) {
 		icone.classList.remove("tema-noite");
 		icone.classList.add("tema-dia");
 		icone.innerText = "light_mode";
-		temaSelecionado = "temaEscuro";
+		mudarCores("temaEscuro");
 	} else {
 		icone.classList.remove("tema-dia");
 		icone.classList.add("tema-noite");
 		icone.innerText = "dark_mode";
-		temaSelecionado = "temaClaro";
+		mudarCores("temaClaro");
 	}
-
-	mudarCores(temaSelecionado);
 }
 botão.addEventListener("click", mudarIcone);
-window.addEventListener("load", mudarIcone); // assim que a página carregar, os estilos carregam junto
+
+function criarProjetos() {
+	const projetos = [
+		{
+			nome: "Landing page",
+			documentação: "https://codepen.io/augusto404/details/bGxyoGV",
+			imagem: "landing-page.png",
+			link: "https://codepen.io/augusto404/full/bGxyoGV"
+		},
+		{
+			nome: "Página de produto",
+			documentação: "https://codepen.io/augusto404/details/KKxbrMw",
+			imagem: "pagina-de-produto.png",
+			link: "https://codepen.io/augusto404/full/KKxbrMw"
+		},
+		{
+			nome: "Login / Cadastro",
+			documentação: "https://codepen.io/augusto404/details/oNPQROp",
+			imagem: "login-e-cadastro.png",
+			link: "https://codepen.io/augusto404/full/oNPQROp"
+		},
+		{
+			nome: "Pomodoro timer",
+			documentação: "https://codepen.io/augusto404/details/ZEMrKMg",
+			imagem: "pomodoro%20timer.png",
+			link: "https://codepen.io/augusto404/full/ZEMrKMg"
+		},
+		{
+			nome: "Link in bio",
+			documentação: "https://github.com/augusto404/augusto404.github.io",
+			imagem: "link-in-bio.png",
+			link: "https://augusto404.github.io"
+		},
+		{
+			nome: "Cartão de crédito",
+			documentação: "https://codepen.io/augusto404/details/abaJLpQ",
+			imagem: "credit-card.png",
+			link: "https://codepen.io/augusto404/full/abaJLpQ"
+		},
+		{
+			nome: "Cadastro em newsletter",
+			documentação: "https://github.com/augusto404/FormularioNewsletter",
+			imagem: "newsletter-astronomia.png",
+			link: "https://augusto404.github.io/FormularioNewsletter"
+		}
+	];
+	const main = document.querySelector("main");
+
+	for (projeto in projetos) {
+		const containerProjeto = `
+ 			<section class="projeto">
+				<img src="img/${projetos[projeto].imagem}">
+				<h2>${projetos[projeto].nome}</h2>
+				<div class="botoes">
+					<a href="${projetos[projeto].documentação}" target="_blank">Detalhes</a>
+					<a href="${projetos[projeto].link}" target="_blank">Visualizar</a>
+				</div>
+			</section>
+ 		`;
+
+		main.innerHTML += containerProjeto;
+	}
+
+	mudarCores();
+}
+window.addEventListener("load", criarProjetos);
